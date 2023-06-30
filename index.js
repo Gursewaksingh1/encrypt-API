03a84fdddaad48d6ad478997a3be0f976159e3d3
Gursewaksingh1 <76276827+Gursewaksingh1@users.noreply.github.com>
2023-06-30 15:44:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,11 @@ const multer = require("multer");
 // const swaggerUI = require("swagger-ui-express");
  const venderAuthRouter = require("./router/auth");
 // const swaggerDocument = require("./swagger");
-const mongoose = require('mongoose');
-const _URI = process.env.MONGODB_URI_ADMIN
-const storage = multer.memoryStorage();
 
 app.use(cors());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(
-  multer({ storage: storage }).fields([
-    { name: "image", maxCount: 10 },
-    { name: "logo", maxCount: 10 },
-    { name: "cover", maxCount: 10 },
-    { name: "banner", maxCount: 10 },
-  ])
-);
- 
+
 app.use((req, res,next) => {
   let host = req.headers.host
   req.subdomian = host.split('.')[0];
@@ -41,11 +30,6 @@ app.use("/", (req, res) => {
 app.use((req, res) => {
   res.send("invalid endpoint please check http method and url");
 });
-
-mongoose.connect(_URI)
-  .then(result => {
-    app.listen(port, () => {
+app.listen(port, () => {
       console.log(`listen on ${port}`);
     });
-  })  
-  .catch(err => console.log(err))
